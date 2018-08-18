ca9356595eb8b2d118db76b9cdfa9b1b0587400e
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 17:55:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
-def batch_badge_creator(array)
-  array.collect{|name| puts "Hello, my name is #{name}"}
+def batch_badge_creator(speakers)
+  speakers.collect{|name| puts "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+  rooms = Array.new(1..7)
 end
